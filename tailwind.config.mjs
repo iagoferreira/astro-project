8cd17ef451aff097ba1718a90c3935f1ebf8618a
Iago Ferreira <iago.cesar@live.com>
2024-03-23 17:37:54 -0300

    add forms

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontSize: {
+        main: "1.2rem",
+        xl: "2.5rem",
+      },
+      colors: {
+        content: "#282828",
+        primary: "#E05346",
+      },
+    },
+  },
+  plugins: [],
 }
